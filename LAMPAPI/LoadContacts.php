4561ce19dbc5939e2bbbb015995f0498b7182428
serrod <serrod@github.com>
2022-02-05 08:01:20 +0000

    commit

--- LAMPAPI/LoadContacts.php
+++ LAMPAPI/LoadContacts.php
@@ -1,47 +1,69 @@
 <?php
-
-include 'Functions.php';
-
-$inData = getRequestInfo();
-
-$searchResults = "";
-$searchCount = 0;
-
-$conn = new mysqli("localhost", "student", "studyhard", "COP4331");
-
-if($conn->connection_error)
-{
-	returnWithError($conn->connection_error);
-}
-else
-{
-//	$result = $conn->query("SELECT * FROM Contacts WHERE UserID ='$UserID';")
-   $stmt = $conn->prepare("SELECT FirstName, LastName/*, Email, Phone*/ FROM Contacts WHERE UserID = ?");
-   $stmt->bind_param("s", $inData["userid"]);
-   $stmt->execute();
-   $result = $stmt->get_result();
-
-   while($row = $result->fetch_assoc())
+	
+   $inData = getRequestInfo();
+  
+   $searchResults = "";
+   $searchCount = 0;
+ 
+   $conn = new mysqli("localhost", "student", "studyhard", "COP4331");
+   if ($conn->connect_error)
    {
-       if( $searchCount > 0 )
+       returnWithError( $conn->connect_error );
+   }
+   else
+   {
+       $stmt = $conn->prepare("SELECT FirstName, LastName FROM Contacts WHERE CONCAT_WS(' ',FirstName,LastName) LIKE ? AND UserID=?");
+       $Name = "%" . $inData["search"] . "%";
+       $stmt->bind_param("ss", $Name, $inData["userid"]);
+       $stmt->execute();
+     
+       $result = $stmt->get_result();
+     
+       while($row = $result->fetch_assoc())
        {
-           $searchResults .= ",";
+           if( $searchCount > 0 )
+           {
+               $searchResults .= ",";
+           }
+           $searchCount++;
+           $searchResults .= '"'.$row["FirstName"] . ' ' . $row["LastName"] .'"';
        }
-       $searchCount++;
-       $searchResults .= '"'.$row["FirstName"] . ' ' . $row["LastName"] .'"';
+      
+       if( $searchCount == 0 )
+       {
+           returnWithError( "No Records Found" );
+       }
+       else
+       {
+           returnWithInfo( $searchResults );
+       }
+      
+       $stmt->close();
+
+	   $conn->close();
    }
 
-    if( $searchCount == 0 )
-    {
-       returnWithError( "No Records Found" );
-    }
-    else
-    {
-        returnWithInfo( $searchResults );
-    }
+   function getRequestInfo()
+   {
+       return json_decode(file_get_contents('php://input'), true);
+   }
+ 
+   function sendResultInfoAsJson( $obj )
+   {
+       header('Content-type: application/json');
+       echo $obj;
+   }
+  
+   function returnWithError( $err )
+   {
+       $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+       sendResultInfoAsJson( $retValue );
+   }
+  
+   function returnWithInfo( $searchResults )
+   {
+       $retValue = '{"results":[' . $searchResults . '],"error":""}';
+       sendResultInfoAsJson( $retValue );
+   }
 
-    stmt->close();
-
-    $conn->close(); 
-}
-   ?>
+?>
