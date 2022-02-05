a6a11192324c1c2b2c0141d07371c9a728d3ae4a
serrod <serrod@github.com>
2022-02-05 08:51:11 +0000

    commit

--- LAMPAPI/LoadContacts.php
+++ LAMPAPI/LoadContacts.php
@@ -64,7 +64,7 @@ function returnWithError( $err )
 
 function returnWithInfo( $searchResults )
 {
-    $retValue = '$searchResults';
+    $retValue = '{"results":[' . $searchResults . ']}';
     sendResultInfoAsJson( $retValue );
 
 }   
