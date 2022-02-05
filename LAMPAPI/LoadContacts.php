7a29f472848ae93a901c60f932e32360a8b220e3
serrod <serrod@github.com>
2022-02-05 09:19:13 +0000

    commit

--- LAMPAPI/LoadContacts.php
+++ LAMPAPI/LoadContacts.php
@@ -73,7 +73,7 @@ function returnWithInfo( $searchResults )
 }  */
 function returnWithInfo( $firstName, $lastName, $Email, $Phone )
 {
-    $retValue = '{'.$firstName.''.$lastName.''.''.$Email.''.$Phone.'}';
+    $retValue = '{'.$firstName. $lastName . $Email . $Phone .'}';
     sendResultInfoAsJson( $retValue );
 } 
 ?>
