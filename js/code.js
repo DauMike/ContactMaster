6d6f2cc4b8f746297962b56050c448f8a6a52d73
serrod <serrod@github.com>
2022-02-06 09:03:11 +0000

    commit

--- js/code.js
+++ js/code.js
@@ -183,7 +183,7 @@ function fillTable()
 
 function display(contactCount, contactFirstName, contactLastName, contactEmail, contactPhone) {
     // get handle on div
-    var container = document.getElementById('container');
+    var container = document.getElementById('fields');
     // create table element
     var table = document.createElement('table');
     var tbody = document.createElement('tbody');
