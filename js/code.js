bc0a6e78b3b712d51f5cae57e2f0202c990b4c0c
serrod <serrod@github.com>
2022-02-06 09:22:37 +0000

    commit

--- js/code.js
+++ js/code.js
@@ -183,11 +183,12 @@ function fillTable()
 
 function display(contactCount, contactFirstName, contactLastName, contactEmail, contactPhone) {
     // get handle on div
-    var container = document.getElementById('contactTable');
+ //   var container = document.getElementById('contactTable');
 //	var container = document.getElementsByClassName('container');
     // create table element
-    var table = document.createElement('table');
-    var tbody = document.createElement('tbody');
+//    var table = document.createElement('table');
+//    var tbody = document.createElement('tbody');
+	var tbody = document.getElementById('contactTable');
     // loop array
 	//alert(contactCount);
     for (i = 0; i < contactCount; i++) {
@@ -226,9 +227,9 @@ function display(contactCount, contactFirstName, contactLastName, contactEmail, 
         tbody.appendChild(row);
     }
     // append tbody to table
-    table.appendChild(tbody);
+ //   table.appendChild(tbody);
     // append table to container
-    container.appendChild(table);
+ //   container.appendChild(table);
 }
 display();
 
