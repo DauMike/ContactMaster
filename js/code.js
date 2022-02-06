077ae24df90f4122cd517bc949eff1ee416e3514
serrod <serrod@github.com>
2022-02-06 08:07:00 +0000

    commit

--- js/code.js
+++ js/code.js
@@ -136,12 +136,12 @@ function loadContacts()
 				contactEmail = jsonObject.emails;
 				contactPhone = jsonObject.phoneNumbers;
 //				fillTable();
-/*
+
 				alert(contactCount);
 				alert(contactFirstName);
 				alert(contactLastName);
 				alert(contactEmail);
-				alert(contactPhone);*/
+				alert(contactPhone);
 				
 			}
 		};
